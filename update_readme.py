6189cada2fb4d31bbcd927d8e6ab559fadf50d1d
pejmankheyri <pejmankheyri@gmail.com>
2024-07-22 17:26:02 +0200

    Update README header with profile age

--- update_readme.py
+++ update_readme.py
@@ -1,5 +1,4 @@
 import datetime
-import re
 
 def calculate_profile_age(creation_date):
     creation_date = datetime.datetime.strptime(creation_date, "%Y-%m-%d")
@@ -11,38 +10,15 @@ def update_readme():
     creation_date = "2013-01-21"  # Replace with your GitHub profile creation date
     years, months = calculate_profile_age(creation_date)
     
-    # with open("README.md", "r") as readme_file:
-    #     existing_content = readme_file.read()
-
     with open("README.md") as f:
         lines = f.readlines()
 
     new_header = f"## Hello World! This is @Pejman 👋 I have been on GitHub for {years} years and {months} months!\n"
 
-    print(lines[0])
     if lines[0] != new_header:
         lines[0] = new_header
         with open("README.md", "w") as f:
             f.writelines(lines)
-
-
-    # new_header = f"## Hello World! This is @Pejman 👋 I have been on GitHub for {years} years and {months} months!\n"
-
-    # replace the first line
-
-    # updated_content = existing_content.replace(first_line, new_header)
-
-    # updated_content = new_header + existing_content
-
-    # Regex to find the existing header and replace it
-    # updated_content = re.sub(r"## Hello World! This is @Pejman 👋 I have been on GitHub for \d+ years and \d+ months!\n", new_header, existing_content)
-
-    # If the header was not found and replaced, prepend it to the content
-    # if updated_content == existing_content:
-    #     updated_content = new_header + existing_content
-    
-    # with open("README.md", "w") as readme_file:
-    #     readme_file.write(updated_content)
 
 if __name__ == "__main__":
     update_readme()
